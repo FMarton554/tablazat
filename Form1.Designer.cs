bd79777fac550f5920e02f18c7bb64a2567d5b01
FMarton554 <fiumeinex@gmail.com>
2021-04-07 15:04:18 +0200

    oraimunka

---
+++ Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿
+namespace datatablegyak
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.torles = new System.Windows.Forms.Button();
+            this.txtTorol = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.reset = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(64, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Autók listája";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(61, 93);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(357, 193);
+            this.dataGridView1.TabIndex = 1;
+            // 
+            // torles
+            // 
+            this.torles.Location = new System.Drawing.Point(343, 53);
+            this.torles.Name = "torles";
+            this.torles.Size = new System.Drawing.Size(75, 23);
+            this.torles.TabIndex = 2;
+            this.torles.Text = "Törlés";
+            this.torles.UseVisualStyleBackColor = true;
+            this.torles.Click += new System.EventHandler(this.torles_Click);
+            // 
+            // txtTorol
+            // 
+            this.txtTorol.Location = new System.Drawing.Point(215, 55);
+            this.txtTorol.Name = "txtTorol";
+            this.txtTorol.Size = new System.Drawing.Size(100, 20);
+            this.txtTorol.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(58, 58);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(102, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Törlendő sor száma:";
+            // 
+            // reset
+            // 
+            this.reset.Location = new System.Drawing.Point(128, 14);
+            this.reset.Name = "reset";
+            this.reset.Size = new System.Drawing.Size(75, 23);
+            this.reset.TabIndex = 5;
+            this.reset.Text = "Újratölt";
+            this.reset.UseVisualStyleBackColor = true;
+            this.reset.Click += new System.EventHandler(this.reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.reset);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTorol);
+            this.Controls.Add(this.torles);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button torles;
+        private System.Windows.Forms.TextBox txtTorol;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button reset;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace datatablegyak
+{
+    // prop tab tab
+
+
+    public partial class Form1 : Form
+    {
+        List<Auto> autokLista = new List<Auto>();
+        DataTable raktarTable;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            autokLista.Add(new Auto("Pál", "BMW", "zöld"));
+            autokLista.Add(new Auto("Petra", "VW", "piros"));
+            autokLista.Add(new Auto("Melinda", "Ford", "fekete"));
+            autokLista.Add(new Auto("Péter", "BMW", "ezüst"));
+            raktarTable = new DataTable();
+
+            tablazatbeallit();
+
+            adatokfeltolt();
+
+            //megjelenítés
+            dataGridView1.DataSource = raktarTable;
+
+        }
+
+        private void adatokfeltolt() {
+     
+            foreach (Auto auto in autokLista)
+            {
+                DataRow dr = raktarTable.NewRow();
+
+                dr["Gyártó"] = auto.Gyarto;
+                dr["Szín"] = auto.Szin;
+                dr["Név"] = auto.Nev;
+
+                raktarTable.Rows.Add(dr);
+         
+            }
+
+
+        }
+
+        public void tablazatbeallit() {
+
+            DataColumn autoIdColumn = new DataColumn("autoId", typeof(int));
+            autoIdColumn.Caption = "Autó ID";
+            autoIdColumn.ReadOnly = true;
+            autoIdColumn.AllowDBNull = false;
+            autoIdColumn.Unique = true;
+            autoIdColumn.AutoIncrement = true;
+            autoIdColumn.AutoIncrementSeed = 0;
+            autoIdColumn.AutoIncrementStep = 1;
+
+
+
+
+            DataColumn gyartoColumn = new DataColumn("Gyártó", typeof(string));
+            DataColumn szinColumn = new DataColumn("Szín", typeof(string));
+            DataColumn nevColumn = new DataColumn("Név", typeof(string));
+
+            raktarTable.Columns.AddRange(new DataColumn[] {
+            autoIdColumn,gyartoColumn,szinColumn,nevColumn});
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void torles_Click(object sender, EventArgs e)
+        {
+
+            try
+            {
+                raktarTable.Rows[int.Parse(txtTorol.Text)].Delete();
+                raktarTable.AcceptChanges();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void reset_Click(object sender, EventArgs e)
+        {
+
+          
+
+        }
+    }
+}
